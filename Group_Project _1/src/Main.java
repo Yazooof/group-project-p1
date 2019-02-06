df31623f8a51465d58d1806ef363db1e68e41c64
Importunity <33600692+Importunity@users.noreply.github.com>
2019-02-05 21:43:06 -0600

    added some comments

--- Group_Project _1/src/Main.java
+++ Group_Project _1/src/Main.java
@@ -16,6 +16,7 @@ public class Main {
         Scanner standardInput = new Scanner(System.in);
         System.out.println("would you like to start collection? 'yes' or 'no' ");
         option = standardInput.nextLine();
+        // option 1 is to ask if the user wants to add onto the json file having it saved.
         if (option.equalsIgnoreCase("yes")) {
             while (option.equalsIgnoreCase("yes")) {
                 JSONWriter writer = new JSONWriter();
@@ -29,6 +30,7 @@ public class Main {
             String option2 = "";
             System.out.println("would you like to start data collection without saving?");
             option2 = standardInput.nextLine();
+            // option2 is for if the user wants to enter in data without having it saved onto the JSON file.
             while (option2.equalsIgnoreCase("yes")) {
                 JSONWriter writer = new JSONWriter();
                 writer.writerInterface();
@@ -41,6 +43,7 @@ public class Main {
 
         }
 
+        // not sure if it's in the criteria but will be a great addition to output the file and display what is needed.
         System.out.println("would you like to read all the reading data?");
         option = standardInput.nextLine();
         if (option.equalsIgnoreCase("yes")) {
